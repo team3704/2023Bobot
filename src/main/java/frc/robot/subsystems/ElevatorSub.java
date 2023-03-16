4300ba141bc1735596874c785f024ba450fe282e
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-03-16 15:30:35 -0700

    getting encoder stuff woooo

--- src/main/java/frc/robot/subsystems/ElevatorSub.java
+++ src/main/java/frc/robot/subsystems/ElevatorSub.java
@@ -3,9 +3,15 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSub extends SubsystemBase {
     private final TalonFX liftMotor = new TalonFX(0);
     private final PIDController liftPidController = new PIDController(0.0000001, 0, 0);
+
+    @Override public void
+    periodic() {
+        SmartDashboard.putNumber("Elevator Encoder", liftMotor.getSelectedSensorPosition());
+    }
 }
