06ad4aadb38316265bcf03f121f508c01e5408bf
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-03-16 15:28:16 -0700

    Refactored Wrist to Arm

---
+++ src/main/java/frc/robot/subsystems/ArmSub.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSub extends SubsystemBase {
+    private final TalonFX
+        motorLeft = new TalonFX(0),
+        motorRight = new TalonFX(0);
+    private final PIDController lifPidController = new PIDController(0.000001, 0, 0);
+
+    @Override public void
+    periodic() {
+        SmartDashboard.putNumber("Left Motor Encoder", motorLeft.getSelectedSensorPosition());
+        SmartDashboard.putNumber("Right Motor Encoder", motorRight.getSelectedSensorPosition());
+    }
+}
