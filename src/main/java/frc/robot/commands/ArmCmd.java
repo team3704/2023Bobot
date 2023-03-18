09f50ff01ad186ed1e20f238de63fc7a56ab1d08
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-03-18 08:52:58 -0700

    Arm stuff

--- src/main/java/frc/robot/commands/ArmCmd.java
+++ src/main/java/frc/robot/commands/ArmCmd.java
@@ -1,7 +1,16 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmSub;
 
 public class ArmCmd extends CommandBase {
-    
+    private final ArmSub arm;
+
+    public ArmCmd(ArmSub arm) {
+        this.arm = arm;
+        addRequirements(arm);
+    }
+
+    @Override public boolean
+    isFinished() {return false;}
 }
