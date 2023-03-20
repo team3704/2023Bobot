6f324e20369cf7057b5bacfdec37ba9132b2bf56
GuavaJavaBoy <vmando867@gmail.com>
2023-03-20 13:59:46 -0700

    drivetrain

--- src/main/java/frc/robot/subsystems/DriveTrainSub.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSub.java
@@ -2,8 +2,24 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
 
 public class DriveTrainSub extends SubsystemBase {
-    TalonFX
+    TalonFX frontleftmotor = new TalonFX(Constants.MotorIds.DT_FrontLeft);
+    TalonFX frontrightmotor = new TalonFX(Constants.MotorIds.DT_FrontRight);
+    TalonFX backleftmotor = new TalonFX(Constants.MotorIds.DT_BackLeft);
+    TalonFX backrightmotor = new TalonFX(Constants.MotorIds.DT_BackRight);
+    DifferentialDrive CtrlDrive = new DifferentialDrive((MotorController) frontleftmotor, (MotorController) frontrightmotor);
+    public DriveTrainSub() {
+        backleftmotor.follow(frontleftmotor);
+        backrightmotor.follow(frontrightmotor);
+    }
+    @Override public void periodic() {
+        CtrlDrive.arcadeDrive(RobotContainer.controller.getLeftY(), RobotContainer.controller.getLeftX());
+    }
+
 }
