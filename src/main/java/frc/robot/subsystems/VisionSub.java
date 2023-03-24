48264ce41c6c3a18f4d4337e40915def01fc88cb
Team 3704 <suhiteam3704@aol.com>
2023-03-23 17:13:59 -0700

    refactored vision to subsystem

--- src/main/java/frc/robot/subsystems/VisionSub.java
+++ src/main/java/frc/robot/subsystems/VisionSub.java
@@ -1,34 +1,35 @@
-package frc.robot;
+package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Vision {    
-    private static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    private static NetworkTableEntry
+public class VisionSub extends SubsystemBase {    
+    private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private NetworkTableEntry
         tx = table.getEntry("tx"),
         ty = table.getEntry("ty"),
         ta = table.getEntry("ta");
     
     // sets limelight settings to defaults
-    static {
+    public VisionSub() {
         table.getEntry("ledMode").setValue(1);
         table.getEntry("camMode").setValue(0);
     }
 
-    private static boolean lights = true;
-    public static void toggleLight() {
+    private boolean lights = true;
+    public void toggleLight() {
         table.getEntry("ledMode").setValue((lights = !lights) ? 3 : 1);
     }
 
-    public static void light(boolean lightSetting) {
+    public void light(boolean lightSetting) {
         lights = lightSetting;
         table.getEntry("ledMode").setValue(lightSetting ? 3 : 1);
     }
 
-    private static boolean visionProcessing = true;
-    public static void toggleCamMode() {
+    private boolean visionProcessing = true;
+    public void toggleCamMode() {
         table.getEntry("camMode").setValue((visionProcessing = !visionProcessing) ? 0 : 1);
     }
 
@@ -36,7 +37,10 @@ public class Vision {
         return Math.abs(getXOffset() / 60) + 0.35;
     }
 
-    public static double getXOffset() {return tx.getDouble(0.0);}
-    public static double getYOffset() {return ty.getDouble(0.0);}
-    public static double getArea() {return ta.getDouble(0.0);}
+    public double getXOffset() {return tx.getDouble(0.0);}
+    public double getYOffset() {return ty.getDouble(0.0);}
+    public double getArea() {return ta.getDouble(0.0);}
+    public void setEntry(String entry, Object value) {
+        table.getEntry(entry).setValue(entry);
+    }
 }
