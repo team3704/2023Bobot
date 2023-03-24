14a8736d3270adc94a3a36f7d5b0f1e90e254206
Team 3704 <suhiteam3704@aol.com>
2023-03-23 17:12:59 -0700

    oops fixed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,10 +25,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_robotContainer.initialize();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    m_robotContainer.initialize();
   }
 
   /**
